dbc0396fcf60740b3bae83805e4c1218ec022761
Thousandspring <66168401+Thousandsprings@users.noreply.github.com>
2021-03-16 16:15:08 +0900

    added urls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello world</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/chiharu-suzuki-915130201/"
+          target="_blank"
+        >
+          Chiharu Suzuki(linkedin)
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Thousandsprings/my-weather-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://upbeat-hypatia-5afe7e.netlify.app/" target="_">
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
