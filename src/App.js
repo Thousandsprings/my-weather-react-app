da734ce2bcb3876225bfbecc33fdc926f6814d2a
Thousandspring <66168401+Thousandsprings@users.noreply.github.com>
2021-03-16 16:40:42 +0900

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,29 +3,32 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/chiharu-suzuki-915130201/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Chiharu Suzuki(linkedin)
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Thousandsprings/my-weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://upbeat-hypatia-5afe7e.netlify.app/" target="_">
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/chiharu-suzuki-915130201/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Chiharu Suzuki(linkedin)
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Thousandsprings/my-weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://upbeat-hypatia-5afe7e.netlify.app/" target="_">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
