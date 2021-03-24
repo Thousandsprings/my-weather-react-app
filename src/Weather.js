bb4f33757960fc7cfac6402e994b00a9b003b9b7
Thousandspring <66168401+Thousandsprings@users.noreply.github.com>
2021-03-24 21:51:39 +0900

    add col and row

--- src/Weather.js
+++ src/Weather.js
@@ -3,19 +3,29 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
-      <h1>16, March. 2021</h1>
-      <h2>
-        <input type="search" placeholder="Enter a city name"></input>
-        <input type="submit" value="Search"></input>
-      </h2>
-      <h3>New York</h3>
-      <ul calssName="info">
-        <li>Tuesday</li>
-        <li>13:00</li>
-        <li>Wind speed</li>
-        <li>Humidity</li>
-      </ul>
+    <div className="Weather container-md">
+      <div className="col-md-6">
+        <h1>New York</h1>
+        <h2>16, March. 2021</h2>
+        <form>
+          <div className="row">
+            <div className="col-md-6">
+              <input type="search" placeholder="Enter a city name"></input>
+            </div>
+            <div className="col-md-3">
+              <input type="submit" value="Search"></input>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="col-md-6">
+        <ul calssName="info">
+          <li>Tuesday</li>
+          <li>13:00</li>
+          <li>Wind speed</li>
+          <li>Humidity</li>
+        </ul>
+      </div>
     </div>
   );
 }
